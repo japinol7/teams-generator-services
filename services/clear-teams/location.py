d85c69f1acedbe130b9043f73e0b48b168c92eb2
japinol <japinol@outlook.com>
2024-09-27 21:42:56 +0200

    [ADD] add-contestants, clear-contestants, clear-teams, list-contestants, list-teams, remove-contestants, reset-teams-generator: Add fake tests to the services that do not have tests yet.

---
+++ services/clear-teams/location.py
@@ -0,0 +1,5 @@
+import os
+
+base_path_location = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
---
+++ services/reset-teams-generator/tests/unit/test_fake.py
@@ -0,0 +1,3 @@
+def test_fake():
+    res = 'test_fake'
+    assert res == 'test_fake'
